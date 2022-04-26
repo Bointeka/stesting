b1b7e2e1940eb9038ae034a0ee83fd0a7d82b7bf
Bointeka <jkaoga@gmail.com>
2022-04-26 03:00:58 -0400

    Able to test html endpoints. Worker endpoint is tested and all 3 pass.

--- src/test/java/com/example/slabiak/appointmentscheduler/tests/BlackBoxTesting.java
+++ src/test/java/com/example/slabiak/appointmentscheduler/tests/BlackBoxTesting.java
@@ -1,42 +1,67 @@
 package com.example.slabiak.appointmentscheduler.tests;
 
 import com.example.slabiak.appointmentscheduler.AppointmentSchedulerApplication;
+import com.example.slabiak.appointmentscheduler.config.WebMvcConfig;
+import com.example.slabiak.appointmentscheduler.security.WebSecurityConfig;
+import com.gargoylesoftware.htmlunit.AjaxController;
+import com.gargoylesoftware.htmlunit.NicelyResynchronizingAjaxController;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.*;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLButtonElement;
+import org.jsoup.nodes.Element;
 import org.junit.Ignore;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.autoconfigure.metrics.MetricsProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.cache.jcache.interceptor.JCacheOperationSourcePointcut;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.htmlunit.MockMvcWebClientBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 //@ExtendWith(SpringExtension.class)
 //@WebMvcTest(controllers = WorkController.class)
 @SpringBootTest( classes = AppointmentSchedulerApplication.class)
 @ActiveProfiles(value = "test")
+@ContextConfiguration(classes = {WebMvcConfig.class, WebSecurityConfig.class})
 @AutoConfigureMockMvc(addFilters = false)
 @WebAppConfiguration
+@Sql({"/appointmentscheduler.sql"})
 public class BlackBoxTesting {
     private MockMvc mockMvc;
+    private WebClient webClient;
 
     @Autowired
     WebApplicationContext webApplicationContext;
 
     @BeforeEach
+
     private void setup () {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).apply(springSecurity()).build();
+        webClient = MockMvcWebClientBuilder.mockMvcSetup(mockMvc).contextPath("").build();
+        webClient.setAjaxController(new AjaxController());
     }
 
     @Nested
@@ -56,6 +81,7 @@ public class BlackBoxTesting {
         //Dont know how to test create new work .
         @Test
         @Order(1)
+        @WithMockUser(roles = {"ADMIN"})
         public void getAllWork() throws Exception {
             String uri = "/" + endPoint + "/all";
             MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get(uri)
@@ -71,7 +97,7 @@ public class BlackBoxTesting {
         }
 
         @Test
-        @Order(3)
+        @Order(2)
         @WithMockUser(roles = {"ADMIN"})
         public void deleteWork() throws Exception {
             String uri = "/" + endPoint + "/delete";
@@ -87,9 +113,49 @@ public class BlackBoxTesting {
                 assertFalse(content.contains(rows));
             }
         }
+
+        @Test
+        @Order(3)
+        @WithMockUser(username = "admin", password = "qwerty123", roles ={"ADMIN"})
+        public void createWork() throws Exception {
+
+            HtmlPage page = webClient.getPage("http://localhost/" + endPoint + "/new");
+
+            List<HtmlForm> form = page.getForms();
+            HtmlPage page1 = null;
+            for (HtmlForm x: form) {
+                if (!x.getActionAttribute().equals("/perform_logout")) {
+                    HtmlTextInput name = x.getInputByName("name");
+                    HtmlNumberInput price = x.getInputByName("price");
+                    HtmlNumberInput duration = x.getInputByName("duration");
+                    HtmlRadioButtonInput target = x.getCheckedRadioButton("targetCustomer");
+                    HtmlRadioButtonInput edit = x.getCheckedRadioButton("editable");
+                    name.setValueAttribute("test");
+                    price.setValueAttribute("100");
+                    duration.setValueAttribute("1");
+                    target.setChecked(true);
+                    edit.setChecked(true);
+                    HtmlButton submit = x.getOneHtmlElementByAttribute("button", "type", "submit");
+                    page1 = submit.click();
+                    webClient.waitForBackgroundJavaScript(10000);
+                    String ro = page1.asXml();
+                    assertTrue(page1.getUrl().toString().contains("/works/all"));
+                    HtmlTable table = page1.getHtmlElementById("works");
+                    for (HtmlTableRow row: table.getRows()) {
+                        if (row.getCell(0).asNormalizedText().contains("test") &&
+                            row.getCell(1).asNormalizedText().contains("retail") &&
+                            row.getCell(2).asNormalizedText().contains("100")) {
+                            return;
+                        }
+                    }
+                    fail();
+                }
+            }
+        }
     }
 
     @Nested
+    @Ignore
     class ProviderTest {
         String endPoint = "providers";
         private String[] htmlBuilder(String nameEmail,String appoinments, int works, int action) {
@@ -205,6 +271,7 @@ public class BlackBoxTesting {
     }
 
     @Nested
+    @Ignore
     class CustomersTest {
         String endPoint = "customers";
         private String[] htmlBuilder(String nameEmail,String type, String appoinments, int action) {
